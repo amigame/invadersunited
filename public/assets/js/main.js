facdc93676d88f6bf0ed8c22dd86fcbd2dbc3e67
Makis Tracend <makis.tracend@gmail.com>
2011-09-25 13:21:50 +1000

    Adding misc folder and other minor adjustments

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,13 @@
 // Variables
 require({ 
     baseUrl: "/assets/js/" 
-	}[	"order!libs/jquery-1.6.4.min", 
+	}, [	
+		"order!libs/jquery-1.6.4.min", 
 		"order!libs/processing-1.3.0.min", 
 		"order!libs/cake", 
+		"order!misc/helpers", 
+		"order!misc/tracking", 
+		"order!config", 
 		"order!objects/sprite", 
 		"order!objects/bullet", 
 		"order!objects/invader", 
@@ -16,8 +20,7 @@ require({
 		"order!logic/opponents", 
 		"order!logic/init", 
 		"order!rooms/lobby", 
-		"order!rooms/arena", 
-		"order!config"
+		"order!rooms/arena"
 	], function () {
 		
 		$(document).ready(function(){
@@ -26,16 +29,6 @@ require({
 			// vector preloads
 			iu.externals.sketch.options.preload = PRELOAD.join(",");
 
-	});
+		});
 });
 
-
-// Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-16164477-4']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
---
+++ public/assets/js/misc/helpers.js
@@ -0,0 +1,17 @@
+// PHP.js functions
+function empty (mixed_var) {
+    var key;
+
+    if (mixed_var === "" || mixed_var === 0 || mixed_var === "0" || mixed_var === null || mixed_var === false || typeof mixed_var === 'undefined') {
+        return true;
+    }
+
+    if (typeof mixed_var == 'object') {
+        for (key in mixed_var) {
+            return false;
+        }
+        return true;
+    }
+
+    return false;
+}
