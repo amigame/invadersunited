e3aeec02f045dc2da79fad507608c95cb5df819c
Makis Tracend <makis.tracend@gmail.com>
2012-05-07 02:03:09 -0700

    Introducing explosions

--- public/assets/js/objects/explosion.js
+++ public/assets/js/objects/explosion.js
@@ -1,39 +1,69 @@
-Explosion = {
-	/*
+Explosion= function() {
+	
+return $.extend({}, (new User()), {
 	root : null, 
 	x : 0, 
 	y : 0, 
 	radius : 0, 
 	
-	circleGradient : new Gradient({
-	type : 'radial',
 	endRadius : 15,
 	colorStops : [
 	  [ 0.0, "rgba(190,105,90,1)" ],
 	  [ 0.25, "rgba(5,30,80,0.4)" ],
 	  [ 1, "rgba(10,0,40,0)" ]
-	]
-	}),
+	],
 	
-	initialize : function(root) {
+	init : function(root) {
 		this.root = root;
+		
 		return this;		
 	},
 	
-	update : function() {
-		this.x = PLAYER.x+(SPRITE_WIDTH/2);
-		this.y = PLAYER.y+(SPRITE_HEIGHT/2);
+	start : function( pos ) {
+		
+		var self = this;
+		
+		this.pos = pos;
+		this.coords();
+		
+		//console.log("!!!EXPLODE!!!");
+		
+		this.expTime = setInterval(function () {
+		  
+			self.update();
+			
+		   if (self.radius > 2*self.endRadius) {
+			   window.clearInterval(self.expTime);
+		   }
+		}, 100);
+		
+	}, 
+	
+	update : function( ) {
+		
+		this.render();
+	}, 
+	
+	render: function(){ 
+		
 		var radius = this.radius;
 		
 		this.root.stroke(0,100-radius*2);
 		this.root.noFill();
 		this.root.ellipse(this.x,this.y,radius,radius);
 		
-		if(this.radius<10){
+		if(this.radius< this.endRadius){
 		  this.radius+=3;
 		}else{
 		  this.radius++;
 		}
-	}
-	*/
+	},
+	
+	coords : function() {
+		this.x = Math.floor( (this.pos.x * SPRITE_WIDTH ) + (SPRITE_WIDTH/2 ) );
+		this.y = Math.floor( (this.pos.y * SPRITE_HEIGHT) + (SPRITE_HEIGHT/2) );
+	} 
+	
+});
+
 }
--- public/assets/js/objects/invader.js
+++ public/assets/js/objects/invader.js
@@ -1,40 +1,46 @@
 Invader = function(){ 
 
 return $.extend({}, (new User()), {
-
+	
 	root : null,
 	style: SPRITE["styles"].invader, 
-    
+    explosion : new Explosion(), 
+	
 	init : function(root){
 		
 		this.root = root;
 		this.active = true;
 		this.type = (this.isAI()) ? "ai" : "player";
-		// set the sprite based on the wave
+		// Initialize sprites
+		// - set the sprite based on the wave
 		var sprite = Math.floor( game.wave.current % SPRITES['invaders'].length );
 		this.sprite = SPRITES['invaders'][sprite];
-		
-		
+		// - explosion
+		this.explosion.init(root);
 	},
 	
 	destroy : function(){
-		if( this.active ) {
+		if( this.active ){ 
 			// reset the active flag
 			this.active = false;
 			// show an explosion
-			//this.explosion.update();
-			// reset pos
-			this.pos = { x: -1, y: -1 };
+			this.explosion.start( this.pos );
+			var self = this;
+			setTimeout(function(){
+				// delete object
+				delete self;
+			}, 2000);
 		}
 	}, 
 	
 	update : function(){
-		
-		this.render();
-		//calculate the 
-		if( neo == player.name && this.type == "ai" && this.isColliding( player.control.bullet ) ){
-			console.log( player.control.bullet );
-			socket.emit("kill-ai", { name: this.name });
+		if( this.active ){ 
+			this.render();
+			//calculate the 
+			if( neo == player.name && this.type == "ai" && this.isColliding( player.control.bullet ) ){
+				console.log( player.control.bullet );
+				socket.emit("kill-ai", { name: this.name });
+			}
 		}
 	}, 
 
