bb5d24b752062d5f3478d02a538e01bd9008e03a
Makis Tracend <makis.tracend@gmail.com>
2015-03-22 06:38:49 -0700

    :zap: express 4.x logic

--- index.js
+++ index.js
@@ -2,70 +2,64 @@
 // Include important JS helpers
 //require( __dirname +'/helpers.js');
 
-var fs = require('fs'), 
-	http = require('http'), 
+var fs = require('fs'),
+	http = require('http'),
 	config = require( __dirname +'/config/app.js'), // get configuration
 	express = require('express'), // Include express engine
-	hbs = require('hbs'), 
+	hbs = require('hbs'),
 	app = express(), // create node server
-	server = http.createServer(app), 
-	io = require('socket.io').listen(server), 
+	server = http.createServer(app),
+	io = require('socket.io').listen(server),
 	iu = require( __dirname +'/lib/game.js');
 
+var DEV = !(process.env.NODE_ENV == 'production');
+
 // Default APP Configuration
-app.configure(function(){
-	app
-		.set('views', __dirname + '/views')
-		.set('view engine', 'html')
-		.engine("html",  require('hbs').__express )
-		.use(app.router);
-		//.use(express.methodOverride());
-});
+app
+	.set('views', __dirname + '/views')
+	.set('view engine', 'html')
+	.engine("html",  require('hbs').__express );
+	//.use(express.methodOverride());
 
-app.configure('development', function(){
+if( DEV ){
 	app
-		.use(express.static(__dirname + '/public'))
-		.use(express.logger())
-		.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  var oneYear = 31557600000;
-		app
-		.use(express.static(__dirname + '/public', { maxAge: oneYear }))
-		.use(express.errorHandler());
-});
+		.use(express.static(__dirname + '/public'));
+} else {
+	var oneYear = 31557600000;
+	app
+		.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+}
 
 // Index Route
-app.get('/', function(req, res){ 
+app.get('/', function(req, res){
 	res.render('index', {
 		locals: {
-			name: 'index', 
+			name: 'index',
 			title: config.name
 		}
 	});
 });
 
 // About page
-app.get('/about', function(req, res){ 
+app.get('/about', function(req, res){
 	res.render('about', {
 		locals: {
-			name: 'about', 
+			name: 'about',
 			title: config.name
 		}
 	});
 });
 
 // Scores page
-app.get('/scores', function(req, res){ 
+app.get('/scores', function(req, res){
 	// consider replacing this "hard" call to the file with a memory seek
 	var file = fs.readFileSync('./data/scores.json');
 	var scores = JSON.parse(file);
-		
+
 	res.render('scores', {
 		locals: {
-			name: 'scores', 
-			title: config.name, 
+			name: 'scores',
+			title: config.name,
 			scores: scores
 		}
 	});
@@ -77,20 +71,20 @@ app.get('/scores', function(req, res){
 var blocks = {};
 
 hbs.registerHelper('extend', function(name, context) {
-    var block = blocks[name];
-    if (!block) {
-        block = blocks[name] = [];
-    }
-	
-    block.push(context.fn(this));
+	var block = blocks[name];
+	if (!block) {
+		block = blocks[name] = [];
+	}
+
+	block.push(context.fn(this));
 });
 
 hbs.registerHelper('block', function(name) {
-    var val = (blocks[name] || []).join('\n');
+	var val = (blocks[name] || []).join('\n');
 
-    // clear the block
-    blocks[name] = [];
-    return val;
+	// clear the block
+	blocks[name] = [];
+	return val;
 });
 
 // start the game
